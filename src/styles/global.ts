436cd829d2a2bba2800b4a54b11358bbb49beff8
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-03-03 19:21:52 -0300

    feat: creates project's color variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    :root {
+        --color-primary: #FF577F;
+        --color-primary-50: #FF427F;
+        --color-primary-disable: #59323F;
+        --grey-4: #121214;
+        --grey-3: #212529;
+        --grey-2: #343B41;
+        --grey-1: #868E96;
+        --grey-0: #F8F9FA;
+        --success: #3FE864;
+        --negative: #E83F5B;
+    }
+    
     * {
         margin: 0;
         padding: 0;
