de2e84391bb8691e79e3caaaa27089f10dfa95e7
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-03-03 19:23:02 -0300

    feat: creates Login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,22 @@
-import { Container } from "./style";
+import { Container, LoginForm } from "./style";
+
+// assets
+import logo from "../../assets/logo.svg";
+
+// components
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 function Login() {
 	return (
 		<Container>
-			<h1>I am the Login page</h1>
+			<img src={logo} alt="logo" />
+			<LoginForm>
+				<h2>Login</h2>
+				<Input label="Email" placeholder="Digite seu email aqui" />
+				<Input label="Senha" placeholder="Digite sua senha aqui" />
+				<Button backgroung="black">Entrar</Button>
+			</LoginForm>
 		</Container>
 	);
 }
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -2,12 +2,23 @@ import styled from "styled-components";
 
 const Container = styled.div`
 	min-height: 100vh;
-	background: black;
+	background: var(--grey-4);
 	color: white;
 
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+
+	img {
+		width: 100px;
+		height: 14px;
+	}
 `;
 
-export { Container };
+const LoginForm = styled.form`
+	background: var(--grey-3);
+	width: 90%;
+`;
+
+export { Container, LoginForm };
