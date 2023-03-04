33bdc7e7971a6459d6528c6dd814b849a501ad56
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-03-04 20:40:08 -0300

    feat: creates Colors class to be able to store the css color variables

---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+class Colors {
+	static colorPrimary: string = "#FF577F";
+	static colorPrimary50: string = "#FF427F";
+	static colorPrimaryDisable: string = "#59323F";
+	static grey4: string = "#121214";
+	static grey3: string = "#212529";
+	static grey2: string = "#343B41";
+	static grey1: string = "#868E96";
+	static grey0: string = "#F8F9FA";
+	static success: string = "#3FE864";
+	static negative: string = "#E83F5B";
+	static white: string = "#ffffff";
+}
+
+export { Colors };
