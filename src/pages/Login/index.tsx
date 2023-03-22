a824a45fbb87a5d72c8e54af9ffa9dbadb75eade
Fernando Scramignon <scramignonnarde@gmail.com>
2023-03-22 18:12:27 -0300

    refactor: removes mock login and registration

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -14,58 +14,51 @@ import { AxiosRequest } from "../../classes/axios";
 import { Colors } from "../../utils";
 
 function Login() {
-	const navigate: NavigateFunction = useNavigate();
+    const navigate: NavigateFunction = useNavigate();
 
-	function navigateToRegisterPage() {
-		navigate("/register");
-	}
+    function navigateToRegisterPage() {
+        navigate("/register");
+    }
 
-	const loginData = {
-		email: "scramignonnarde@gmail.com",
-		password: "1234",
-	};
+    const loginData = {
+        email: "scramignonnarde@gmail.com",
+        password: "1234",
+    };
 
-	return (
-		<Container>
-			<img src={logo} alt="logo" />
-			<LoginForm
-				onClick={(e) =>
-					AxiosRequest.loginRequest(
-						loginData.email,
-						loginData.password
-					)
-				}
-			>
-				<h2>Login</h2>
-				<Input label="Email" placeholder="Digite seu email aqui" />
-				<Input
-					label="Senha"
-					placeholder="Digite sua senha aqui"
-					type="password"
-				/>
-				<Button
-					background={Colors.colorPrimary}
-					hoverBackground={Colors.colorPrimary50}
-					color={Colors.white}
-				>
-					Entrar
-				</Button>
-				<RegisterSection>
-					<span onClick={navigateToRegisterPage}>
-						Ainda não possuí uma conta?
-					</span>
-					<Button
-						clickFunction={navigateToRegisterPage}
-						background={Colors.grey1}
-						hoverBackground={Colors.grey2}
-						color={Colors.white}
-					>
-						Cadastre-se
-					</Button>
-				</RegisterSection>
-			</LoginForm>
-		</Container>
-	);
+    return (
+        <Container>
+            <img src={logo} alt="logo" />
+            <LoginForm onClick={(e) => console.log("Login goes here")}>
+                <h2>Login</h2>
+                <Input label="Email" placeholder="Digite seu email aqui" />
+                <Input
+                    label="Senha"
+                    placeholder="Digite sua senha aqui"
+                    type="password"
+                />
+                <Button
+                    background={Colors.colorPrimary}
+                    hoverBackground={Colors.colorPrimary50}
+                    color={Colors.white}
+                >
+                    Entrar
+                </Button>
+                <RegisterSection>
+                    <span onClick={navigateToRegisterPage}>
+                        Ainda não possuí uma conta?
+                    </span>
+                    <Button
+                        clickFunction={navigateToRegisterPage}
+                        background={Colors.grey1}
+                        hoverBackground={Colors.grey2}
+                        color={Colors.white}
+                    >
+                        Cadastre-se
+                    </Button>
+                </RegisterSection>
+            </LoginForm>
+        </Container>
+    );
 }
 
 export default Login;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,9 @@
 import {
-	Container,
-	RegisterHeader,
-	RegisterForm,
-	FormTitleDiv,
-	FormInputDiv,
+    Container,
+    RegisterHeader,
+    RegisterForm,
+    FormTitleDiv,
+    FormInputDiv,
 } from "./style";
 
 import { NavigateFunction, useNavigate } from "react-router-dom";
@@ -19,78 +19,68 @@ import { courseModuleTypes } from "../../interfaces/Login";
 import { Colors } from "../../utils";
 
 function Register() {
-	const navigate: NavigateFunction = useNavigate();
-	function navigatetoLogin(): void {
-		navigate("/");
-	}
-	const moduleOptions = [
-		"Primeiro módulo",
-		"Segundo módulo",
-		"Terceiro módulo",
-		"Quarto módulo",
-	];
+    const navigate: NavigateFunction = useNavigate();
+    function navigatetoLogin(): void {
+        navigate("/");
+    }
+    const moduleOptions = [
+        "Primeiro módulo",
+        "Segundo módulo",
+        "Terceiro módulo",
+        "Quarto módulo",
+    ];
 
-	const registerData = {
-		name: "Fernando",
-		email: "scramignonnarde6@gmail.com",
-		contact: "someLinkedin2",
-		password: "1234",
-		course_module: courseModuleTypes.FirstModule,
-		bio: "Eu sou o Fernando",
-		avatar_url: "www.url.com",
-	};
-
-	return (
-		<Container>
-			<RegisterHeader>
-				<img src={logo} alt="logo" onClick={navigatetoLogin} />
-				<Button
-					clickFunction={navigatetoLogin}
-					color={Colors.grey0}
-					background={Colors.grey3}
-					hoverBackground={Colors.grey2}
-				>
-					Voltar
-				</Button>
-			</RegisterHeader>
-			<RegisterForm
-				onClick={(e) => AxiosRequest.registerRequest(registerData)}
-			>
-				<FormTitleDiv>
-					<h2>Crie sua conta</h2>
-					<p>Rapido e grátis, vamos nessa</p>
-				</FormTitleDiv>
-				<FormInputDiv>
-					<Input label="Nome" placeholder="Digite aqui seu nome" />
-					<Input label="Email" placeholder="Digite aqui seu email" />
-					<Input
-						label="Senha"
-						placeholder="Digite aqui sua senha"
-						type="password"
-					/>
-					<Input
-						label="Confirmar senha"
-						placeholder="Digite sua senha novamente"
-						type="password"
-					/>
-					<Input label="Bio" placeholder="Fale sobre você" />
-					<Input label="Contato" placeholder="Opção de contato" />
-					<Select
-						name="modules-select"
-						label="Selecionar módulo"
-						options={moduleOptions}
-					/>
-					<Button
-						background={Colors.colorPrimaryDisable}
-						hoverBackground={Colors.colorPrimary}
-						color="white"
-					>
-						Cadastrar
-					</Button>
-				</FormInputDiv>
-			</RegisterForm>
-		</Container>
-	);
+    return (
+        <Container>
+            <RegisterHeader>
+                <img src={logo} alt="logo" onClick={navigatetoLogin} />
+                <Button
+                    clickFunction={navigatetoLogin}
+                    color={Colors.grey0}
+                    background={Colors.grey3}
+                    hoverBackground={Colors.grey2}
+                >
+                    Voltar
+                </Button>
+            </RegisterHeader>
+            <RegisterForm
+                onClick={(e) => console.log("Registration goes here")}
+            >
+                <FormTitleDiv>
+                    <h2>Crie sua conta</h2>
+                    <p>Rapido e grátis, vamos nessa</p>
+                </FormTitleDiv>
+                <FormInputDiv>
+                    <Input label="Nome" placeholder="Digite aqui seu nome" />
+                    <Input label="Email" placeholder="Digite aqui seu email" />
+                    <Input
+                        label="Senha"
+                        placeholder="Digite aqui sua senha"
+                        type="password"
+                    />
+                    <Input
+                        label="Confirmar senha"
+                        placeholder="Digite sua senha novamente"
+                        type="password"
+                    />
+                    <Input label="Bio" placeholder="Fale sobre você" />
+                    <Input label="Contato" placeholder="Opção de contato" />
+                    <Select
+                        name="modules-select"
+                        label="Selecionar módulo"
+                        options={moduleOptions}
+                    />
+                    <Button
+                        background={Colors.colorPrimaryDisable}
+                        hoverBackground={Colors.colorPrimary}
+                        color="white"
+                    >
+                        Cadastrar
+                    </Button>
+                </FormInputDiv>
+            </RegisterForm>
+        </Container>
+    );
 }
 
 export default Register;
