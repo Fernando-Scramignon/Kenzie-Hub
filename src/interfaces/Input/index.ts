10daaa99922029b8b0fabd2b52b2a1fa4cd610b1
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-12 15:31:17 -0300

    fix: fixes register typing and name options

--- src/interfaces/Input/index.ts
+++ src/interfaces/Input/index.ts
@@ -1,11 +1,19 @@
 import { ReactNode } from "react";
 import { UseFormRegister, FieldErrors, FieldValues } from "react-hook-form";
+import { IRegister } from "../Login";
 
 interface IInput {
     label: string;
-    name: string;
-    register?: UseFormRegister<FieldValues>;
-    errors?: FieldErrors<FieldValues>;
+    name:
+        | "name"
+        | "email"
+        | "contact"
+        | "bio"
+        | "password"
+        | "course_module"
+        | "passwordMatch";
+    register?: UseFormRegister<IRegister>;
+    errors?: FieldErrors<IRegister>;
     placeholder: string;
     type?: "default" | "password";
 }
