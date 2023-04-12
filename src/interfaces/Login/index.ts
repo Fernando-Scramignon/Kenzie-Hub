9cb2a454c12d4f78a680bf2ff4aa0fc1997b987e
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-12 15:30:47 -0300

    fix: adds passwordMatch to Register interface

--- src/interfaces/Login/index.ts
+++ src/interfaces/Login/index.ts
@@ -1,18 +1,19 @@
 enum courseModuleTypes {
-	FirstModule = "Primeiro módulo",
-	SecondModule = "Segundo módulo",
-	ThirdModule = "Terceiro módulo",
-	FourthModule = "Quarto módulo",
+    FirstModule = "Primeiro módulo",
+    SecondModule = "Segundo módulo",
+    ThirdModule = "Terceiro módulo",
+    FourthModule = "Quarto módulo",
 }
 
 interface IRegister {
-	email: string;
-	password: string;
-	name: string;
-	contact: string;
-	course_module?: courseModuleTypes;
-	bio?: string;
-	avatar_url: string;
+    email: string;
+    password: string;
+    passwordMatch: string;
+    name: string;
+    contact: string;
+    course_module?: courseModuleTypes;
+    bio?: string;
+    avatar_url: string;
 }
 
 export type { IRegister };
