a2ee6a8e1646fbb47b11e1472f0c69bc1b9b09e9
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-12 15:24:49 -0300

    feat: adds course modules to schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-import { moduleOptions } from "../pages/Register";
+// import { moduleOptions } from "../pages/Register";
 
 function maxCharMessage(number: number): string {
     return `Máximo de ${number} caracteres`;
@@ -10,40 +10,46 @@ function requiredMessage(name: string): string {
     return `${name} é obrigatório`;
 }
 
-const PASSWORD_REGEX: RegExp =
-    /^(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=D*d)(?=[^!#%]*[!#%])[A-Za-z0-9!#%]{8,32}$/;
+const moduleOptions = [
+    "Primeiro módulo",
+    "Segundo módulo",
+    "Terceiro módulo",
+    "Quarto módulo",
+];
+
+const PASSWORD_REGEX: RegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 const userSchema = yup.object({
     name: yup
         .string()
-        .required(requiredMessage("name"))
+        .required(requiredMessage("Nome"))
         .max(256, maxCharMessage(256)),
     email: yup
         .string()
         .required(requiredMessage("E-mail"))
-        .length(256, maxCharMessage(256))
+        .max(256, maxCharMessage(256))
         .email("Formato de E-mail inválido"),
     bio: yup
         .string()
         .required(requiredMessage("Bio"))
-        .length(512, maxCharMessage(512)),
+        .max(512, maxCharMessage(512)),
     contact: yup
         .string()
         .required(requiredMessage("Contato"))
-        .length(256, maxCharMessage(256)),
+        .max(256, maxCharMessage(256)),
     password: yup
         .string()
         .required(requiredMessage("Senha"))
-        .length(256, maxCharMessage(256))
+        .max(256, maxCharMessage(256))
         .matches(PASSWORD_REGEX, "Senha em formato inválido"),
     passwordMatch: yup
         .string()
-        .required(requiredMessage("Confirmação de senha"))
+        .required(requiredMessage("Confirmação"))
         .oneOf([yup.ref("password")], "Senhas tem que ser iguais"),
-    // course_module: yup
-    //     .mixed()
-    //     .oneOf(Object.values(moduleOptions))
-    //     .required("Módulo é obrigatório"),
+    course_module: yup
+        .mixed()
+        .oneOf(Object.values(moduleOptions))
+        .required("Módulo é obrigatório"),
 });
 
-export { userSchema };
+export { userSchema, moduleOptions };
