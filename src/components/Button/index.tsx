7fca72558982af6734c885818df22b76e2252965
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-21 10:12:40 -0300

    feat: adds the id prop to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,22 +3,24 @@ import { Container } from "./style";
 import { IButton } from "../../interfaces/Button";
 
 function Button({
-	background,
-	children,
-	hoverBackground = background,
-	color = "white",
-	clickFunction,
+    background,
+    children,
+    hoverBackground = background,
+    color = "white",
+    id,
+    clickFunction,
 }: IButton) {
-	return (
-		<Container
-			onClick={() => clickFunction && clickFunction()}
-			background={background}
-			color={color}
-			hoverBackground={hoverBackground}
-		>
-			{children}
-		</Container>
-	);
+    return (
+        <Container
+            onClick={() => clickFunction && clickFunction()}
+            background={background}
+            color={color}
+            hoverBackground={hoverBackground}
+            id={id}
+        >
+            {children}
+        </Container>
+    );
 }
 
 export default Button;
--- src/interfaces/Button/index.tsx
+++ src/interfaces/Button/index.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
 
 interface IButton {
-	background: string;
-	hoverBackground?: string;
-	color?: string;
-	clickFunction?: Function;
-	children: ReactNode;
+    background: string;
+    hoverBackground?: string;
+    color?: string;
+    clickFunction?: Function;
+    children: ReactNode;
+    id?: string;
 }
 
 export type { IButton };
