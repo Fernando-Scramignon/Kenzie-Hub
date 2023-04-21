acee11cb1a0b2dff72fb487460ce9084ed0d20b0
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-21 09:38:58 -0300

    feat: starts the creation of the home component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./style";
+
+function Home() {
+    return (
+        <Container>
+            <h1>Home sweet home</h1>
+        </Container>
+    );
+}
+
+export default Home;
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    min-height: 100vh;
+    background: var(--grey-4);
+    color: white;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export { Container };
