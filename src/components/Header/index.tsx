22aed368f05b6b435cde2c5de6a9136e697b2b91
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-21 10:25:15 -0300

    feat: makes logout button functional

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,16 @@ import Button from "../Button";
 import logo from "../../assets/logo.svg";
 import { Colors } from "../../utils";
 
+import { useNavigate, NavigateFunction } from "react-router-dom";
+
 function Header() {
+    const navigate: NavigateFunction = useNavigate();
+
+    function logout(): void {
+        localStorage.clear();
+        navigate("/login/");
+    }
+
     return (
         <Container>
             <img id="home-logo" src={logo} alt="kenzie hub logo" />
@@ -14,6 +23,7 @@ function Header() {
                 background={Colors.grey3}
                 hoverBackground={Colors.grey2}
                 color={Colors.grey0}
+                clickFunction={logout}
             >
                 Sair
             </Button>
