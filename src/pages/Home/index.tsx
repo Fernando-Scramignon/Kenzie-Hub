6e48891137fbab7dfc78416b5feafa1cd8f366c8
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-22 08:50:59 -0300

    feat: makes Home page only acessible for logged users

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,23 @@
 import { Container } from "./style";
 
 import Header from "../../components/Header";
+import { useEffect, useState } from "react";
+import { IUser } from "../../interfaces/User";
+import { useNavigate, NavigateFunction } from "react-router-dom";
 
 function Home() {
+    const navigate: NavigateFunction = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate("/login/");
+            return;
+        }
+    }, []);
+
+    const [user, setUser] = useState<IUser>();
+
     return (
         <Container>
             <Header />
