8da4d14ace6828298e1ea46e9dc79a0f497f5572
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-22 10:03:00 -0300

    feat: creates user list own info route

--- src/classes/axios.ts
+++ src/classes/axios.ts
@@ -1,13 +1,34 @@
 import axios from "axios";
 
 import { IRegister } from "../interfaces/Login";
+import { IUser } from "../interfaces/User";
 
 class AxiosRequest {
     static BASE_URL = "http://localhost:8000/api/";
 
     static LOGIN_PATH: string = "login/";
 
+    static GET_USER_PATH: string = "users/";
+
     static REGISTER_PATH: string = "users/";
+
+    static async getUser(token: string): Promise<IUser> {
+        const options = {
+            method: "GET",
+            url: `${this.BASE_URL}${this.GET_USER_PATH}`,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        const response: Promise<any> = axios
+            .request(options)
+            .then((res) => res)
+            .catch((error) => error);
+
+        return response;
+    }
 
     static async loginRequest(email: string, password: string): Promise<any> {
         const options = {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import { IUser } from "../../interfaces/User";
 import { useNavigate, NavigateFunction } from "react-router-dom";
 
+import { AxiosRequest } from "../../classes/axios";
+
 function Home() {
     const navigate: NavigateFunction = useNavigate();
 
@@ -16,12 +18,18 @@ function Home() {
         }
     }, []);
 
+    async function testGetUser(): Promise<void> {
+        let token: string = localStorage.getItem("token")!;
+        const res: any = await AxiosRequest.getUser(token);
+        console.log(res);
+    }
+
     const [user, setUser] = useState<IUser>();
 
     return (
         <Container>
             <Header />
-            <h1>Home sweet home</h1>
+            <h1 onClick={testGetUser}>Home sweet home</h1>
             <span>under construction</span>
         </Container>
     );
