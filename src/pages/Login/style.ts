e16f063fc47d8a6c0766784a84ac401e5dcebca2
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-24 18:19:43 -0300

    feat: adds animations to login and registration page

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,74 +1,78 @@
 import styled from "styled-components";
+import { appearFromBottom, fadeIn } from "../../styles/animations";
 
 const Container = styled.div`
-	min-height: 100vh;
+    min-height: 100vh;
 
-	background: var(--grey-4);
-	color: white;
+    background: var(--grey-4);
+    color: white;
 
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
 
-	img {
-		height: 18px;
-	}
+    img {
+        height: 18px;
+        animation: ${fadeIn} 1.5s;
+    }
 `;
 
 const LoginForm = styled.form`
-	width: 90%;
-	max-width: 370px;
-	padding: 33px 17px;
+    width: 90%;
+    max-width: 370px;
+    padding: 33px 17px;
 
-	background: var(--grey-3);
+    background: var(--grey-3);
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
 
-	box-shadow: 0px 3.2px 32.1px -8px #00000040;
-	border-radius: 3.2px;
+    box-shadow: 0px 3.2px 32.1px -8px #00000040;
+    border-radius: 3.2px;
 
-	h2 {
-		font-size: 16px;
-		font-weight: 700;
-	}
+    animation: ${appearFromBottom} 1s;
 
-	@media (min-width: 1024px) {
-		h2 {
-			font-size: 22px;
-		}
-	}
+    h2 {
+        font-size: 16px;
+        font-weight: 700;
+    }
+
+    @media (min-width: 1024px) {
+        h2 {
+            font-size: 22px;
+        }
+    }
 `;
 
 const RegisterSection = styled.div`
-	width: 100%;
+    width: 100%;
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 17px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 17px;
 
-	span {
-		font-family: "Inter", sans-serif;
-		font-weight: 600;
-		color: var(--grey-1);
-		font-size: 9px;
+    span {
+        font-family: "Inter", sans-serif;
+        font-weight: 600;
+        color: var(--grey-1);
+        font-size: 9px;
 
-		&:hover {
-			text-decoration: underline;
-			cursor: pointer;
-		}
-	}
+        &:hover {
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    }
 
-	@media (min-width: 1024px) {
-		span {
-			font-size: 12px;
-		}
-	}
+    @media (min-width: 1024px) {
+        span {
+            font-size: 12px;
+        }
+    }
 `;
 
 export { Container, LoginForm, RegisterSection };
--- src/pages/Register/style.ts
+++ src/pages/Register/style.ts
@@ -1,97 +1,102 @@
 import styled from "styled-components";
+import { appearFromBottom, fadeIn } from "../../styles/animations";
 
 const Container = styled.div`
-	min-height: 100vh;
-	padding-top: 40px;
-	padding-bottom: 40px;
+    min-height: 100vh;
+    padding-top: 40px;
+    padding-bottom: 40px;
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	gap: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
 
-	background: var(--grey-4);
+    background: var(--grey-4);
 `;
 
 const RegisterHeader = styled.header`
-	width: 90%;
-	max-width: 370px;
+    width: 90%;
+    max-width: 370px;
 
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-	img {
-		height: 18px;
-		cursor: pointer;
-	}
+    animation: ${fadeIn} 1.5s;
 
-	button {
-		width: 100px;
-		font-weight: 600;
-	}
+    img {
+        height: 18px;
+        cursor: pointer;
+    }
 
-	@media (min-width: 1024px) {
-		img {
-			height: 20px;
-		}
+    button {
+        width: 100px;
+        font-weight: 600;
+    }
 
-		button {
-			font-size: 14px;
-		}
-	}
+    @media (min-width: 1024px) {
+        img {
+            height: 20px;
+        }
+
+        button {
+            font-size: 14px;
+        }
+    }
 `;
 
 const RegisterForm = styled.form`
-	width: 90%;
-	max-width: 370px;
-	padding: 18px;
-	border-radius: 3.2px;
+    width: 90%;
+    max-width: 370px;
+    padding: 18px;
+    border-radius: 3.2px;
 
-	background: var(--grey-3);
+    background: var(--grey-3);
 
-	color: var(--grey-0);
+    color: var(--grey-0);
 
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 26px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 26px;
+
+    animation: ${appearFromBottom} 1s;
 `;
 
 const FormTitleDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 17px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 17px;
 
-	h2 {
-		font-weight: 700;
-		font-size: 16px;
-	}
+    h2 {
+        font-weight: 700;
+        font-size: 16px;
+    }
 
-	p {
-		font-size: 10px;
-		color: var(--grey-1);
-	}
+    p {
+        font-size: 10px;
+        color: var(--grey-1);
+    }
 
-	@media (min-width: 1024px) {
-		h2 {
-			font-size: 20px;
-		}
+    @media (min-width: 1024px) {
+        h2 {
+            font-size: 20px;
+        }
 
-		p {
-			font-size: 12px;
-		}
-	}
+        p {
+            font-size: 12px;
+        }
+    }
 `;
 
 const FormInputDiv = styled.div`
-	width: 100%;
+    width: 100%;
 
-	display: flex;
-	flex-direction: column;
-	gap: 22px;
+    display: flex;
+    flex-direction: column;
+    gap: 22px;
 `;
 
 export { Container, RegisterHeader, RegisterForm, FormTitleDiv, FormInputDiv };
