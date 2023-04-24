da47fa97c424f223fd0a3d6b1cad1f9d79d587ee
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-24 18:46:35 -0300

    feat: adds appear from right and top animations

--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -34,4 +34,35 @@ to {
 }
 `;
 
-export { fadeIn, appearFromLeft, appearFromBottom };
+const appearFromRight: Keyframes = keyframes`
+from {
+    opacity: 0;
+    transform: translateX(50px);
+}
+
+to {
+    opacity: 1;
+    transform: translateX(0px);
+}
+`;
+
+const appearFromTop: Keyframes = keyframes`
+from {
+    opacity: 0;
+    transform: translateY(-50px);
+}
+
+to {
+    opacity: 1;
+    transform: translateY(0px);
+}
+
+`;
+
+export {
+    fadeIn,
+    appearFromLeft,
+    appearFromBottom,
+    appearFromRight,
+    appearFromTop,
+};
