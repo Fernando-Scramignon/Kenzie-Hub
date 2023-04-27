a45d3e3aa20a1f792d57395df5ce4739aaea1f51
Fernando Scramignon <scramignonnarde@gmail.com>
2023-04-27 10:26:30 -0300

    feat: makes register tech modal alternable

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Container, TechSection } from "./style";
 import Header from "../../components/Header";
 import Info from "../../components/Info";
 import Tech from "../../components/Tech";
-import { RegisterTechModal } from "../../components/RegisterTechModal";
+import RegisterTechModal from "../../components/RegisterTechModal";
 
 import { useEffect, useState } from "react";
 import { ITech, IUser } from "../../interfaces/User";
@@ -13,7 +13,12 @@ import { AxiosRequest } from "../../classes/axios";
 
 function Home() {
     const [user, setUser] = useState<IUser>();
+    const [isTechCreationOpen, setIsTechCreationOpen] = useState<boolean>();
     const navigate: NavigateFunction = useNavigate();
+
+    function alternateIsTechCreationOpen(): void {
+        setIsTechCreationOpen(!isTechCreationOpen);
+    }
 
     useEffect(() => {
         const token: string | null = localStorage.getItem("token");
@@ -37,7 +42,12 @@ function Home() {
             <TechSection>
                 <div className="tech-title">
                     <h3>Tecnologias</h3>
-                    <span id="add-tech-btn">+</span>
+                    <span
+                        id="add-tech-btn"
+                        onClick={alternateIsTechCreationOpen}
+                    >
+                        +
+                    </span>
                 </div>
                 {user?.techs.length != 0 && (
                     <div className="tech-list">
@@ -47,7 +57,7 @@ function Home() {
                     </div>
                 )}
             </TechSection>
-            {<RegisterTechModal />}
+            {isTechCreationOpen && <RegisterTechModal />}
         </Container>
     );
 }
