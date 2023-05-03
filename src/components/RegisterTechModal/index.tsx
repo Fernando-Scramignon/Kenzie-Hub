d2e73b2eefe443c0026800a7c5643a5a9f8ab5d6
Fernando Scramignon <scramignonnarde@gmail.com>
2023-05-03 19:51:44 -0300

    feat: implements register tech feature to register tech modal

--- src/components/RegisterTechModal/index.tsx
+++ src/components/RegisterTechModal/index.tsx
@@ -3,14 +3,33 @@ import Input from "../Input";
 import Select from "../Select";
 import { CSSTransition } from "react-transition-group";
 import { IRegisterTechModal } from "../../interfaces/Tech";
-import { STATUS_OPTIONS } from "../../schemas/tech.schemas";
+import { IRegisterTech } from "../../interfaces/Tech";
+import { STATUS_OPTIONS, techSchema } from "../../schemas/tech.schemas";
 import Button from "../Button";
 import { Colors } from "../../utils";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { AxiosRequest } from "../../classes/axios";
 
 function RegisterTechModal({
     showTechCreation,
     alternateTechCreation,
+    updateUserFunction,
 }: IRegisterTechModal) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IRegisterTech>({
+        resolver: yupResolver(techSchema),
+    });
+
+    async function onSubmitFunction(data: IRegisterTech): Promise<void> {
+        const response: Promise<any> = await AxiosRequest.registerTech(data);
+        updateUserFunction();
+        alternateTechCreation();
+    }
+
     return (
         <CSSTransition
             in={showTechCreation}
@@ -25,15 +44,21 @@ function RegisterTechModal({
                         <p>Cadastrar Tecnologia</p>
                         <span onClick={alternateTechCreation}>X</span>
                     </div>
-                    <div className="register-tech-body">
+                    <form
+                        className="register-tech-body"
+                        onSubmit={handleSubmit(onSubmitFunction)}
+                    >
                         <Input
                             label="Nome"
                             name="title"
+                            register={register}
+                            errors={errors}
                             placeholder="Nome da tecnologia"
                         />
                         <Select
                             label="Selecionar status"
                             name="status"
+                            register={register}
                             options={STATUS_OPTIONS}
                         />
                         <Button
@@ -43,7 +68,7 @@ function RegisterTechModal({
                         >
                             Cadastrar tecnologia
                         </Button>
-                    </div>
+                    </form>
                 </Container>
                 <Backdrop />
             </>
--- src/interfaces/Tech/index.ts
+++ src/interfaces/Tech/index.ts
@@ -4,9 +4,15 @@ interface ITechProps {
     tech: ITech;
 }
 
+interface IRegisterTech {
+    title: string;
+    status: string;
+}
+
 interface IRegisterTechModal {
     showTechCreation: boolean;
     alternateTechCreation(): void;
+    updateUserFunction(): void;
 }
 
-export type { ITechProps, IRegisterTechModal };
+export type { ITechProps, IRegisterTechModal, IRegisterTech };
